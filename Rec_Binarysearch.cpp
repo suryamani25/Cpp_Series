935b4e08810b7d282f11f9f14581c7f7444fd379
Surya Mani Verma <93043048+suryamani25@users.noreply.github.com>
2025-06-28 22:59:24 +0530

    Binary Search using Recursion in C++

---
+++ Rec_Binarysearch.cpp
@@ -0,0 +1,37 @@
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+int BSR(int arr[], int low, int high , int target){
+    if(low > high)
+    return -1;
+    int mid = (low + high) / 2;
+    if(arr[mid] == target)
+    return mid;
+    else if(arr[mid] > high)
+    return BSR(arr,mid + 1,high,target);
+    else
+    return BSR(arr,low,mid-1,target);
+}
+
+int main() {
+    int n;
+    cout<< "Enter number of Elements in Sorted Array: ";
+    cin>>n;
+    int arr[n];
+    cout<< "Enter "<<n<< " Elements in Sorted Array:\n";
+    for(int i =0 ; i<n ; i++){
+        cin >> arr[i];
+    }
+    int target;
+    cout<<"Enter the elements to search : ";
+    cin >> target;
+    
+    int result = BSR(arr,0,n-1,target);
+    if(result != -1)
+    cout<<"Element found at Index : " << result <<endl;
+    else
+    cout<< "Element not found";
+
+    return 0;
+}
